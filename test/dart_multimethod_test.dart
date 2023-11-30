f74cb8501277479d98b0d1db2b6836ed501a816e
Ryoichi Izumita <ryoichi.izumita@codetakt.com>
2023-11-30 21:48:46 +0900

    Update pubspec.yaml

--- test/dart_multimethod_test.dart
+++ test/dart_multimethod_test.dart
@@ -1,5 +1,5 @@
 import 'package:dart_multimethod/dart_multimethod.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('Multimethod', () {
